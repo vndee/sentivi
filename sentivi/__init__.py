e4267d78faea27a25693c30751da69b9d2c4d943
vndee <hvd.huynhduy@gmail.com>
2020-09-28 16:27:40 +0700

    remove redundant code

--- sentivi/__init__.py
+++ sentivi/__init__.py
@@ -1,3 +1,2 @@
-from .corpus import Corpus
 from .text_processor import TextProcessor
 from .pipeline import Pipeline
--- test/vocab.py
+++
@@ -1,7 +0,0 @@
-from sentivi.corpus import Corpus
-
-if __name__ == '__main__':
-    corpus = Corpus(source_file='./data/dev.vi', n_grams=3)
-    # corpus.export_vocab('./data/vocab.vi')
-    for x, y in corpus:
-        print(x, y)
