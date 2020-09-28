4f20c7dd6ca4789a0542b504d423c396afab08b3
vndee <hvd.huynhduy@gmail.com>
2020-09-28 16:31:18 +0700

    update transformer

--- sentivi/classifier/__init__.py
+++ sentivi/classifier/__init__.py
@@ -1,20 +1,10 @@
-from sentivi.base_model import ClassifierLayer
-from .naive_bayes import NaiveBayesClassifier
 from .svm import SVMClassifier
 from .mlp import MLPClassifier
-from .decision_tree import DecisionTreeClassifier
 from .sgd import SGDClassifier
+from .lstm import LSTMClassifier
+from .text_cnn import TextCNNClassifier
+from .transformer import TransformerClassifier
+from .naive_bayes import NaiveBayesClassifier
+from .decision_tree import DecisionTreeClassifier
 from .nearest_centroid import NearestCentroidClassifier
 from .gaussian_process import GaussianProcessClassifier
-from .text_cnn import TextCNNClassifier
-from .lstm import LSTMClassifier
-
-
-class RNNClassifier(ClassifierLayer):
-    def __init__(self):
-        super(RNNClassifier, self).__init__()
-
-
-class TransformerClassifier(ClassifierLayer):
-    def __init__(self):
-        super(TransformerClassifier, self).__init__()
---
+++ sentivi/classifier/transformer.py
@@ -0,0 +1,6 @@
+from sentivi.base_model import ClassifierLayer
+
+
+class TransformerClassifier(ClassifierLayer):
+    def __init__(self):
+        super(TransformerClassifier, self).__init__()
