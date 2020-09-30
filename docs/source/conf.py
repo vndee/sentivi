313561fbbd584f9135e311d22d7821cae0c60e4f
vndee <hvd.huynhduy@gmail.com>
2020-09-30 09:43:28 +0700

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,7 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['static']
 
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
     'special-members': '__init__',
-    'undoc-members': True,
-    'exclude-members': '__weakref__'
 }
 
 sys.path.insert(0, os.path.abspath('../..'))
