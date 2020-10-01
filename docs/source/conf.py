cedf281a1ef4e90ca0555c0d5a7414a032efada6
vndee <hvd.huynhduy@gmail.com>
2020-10-01 14:48:52 +0700

    update docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,8 +4,8 @@ import sys
 project = 'Sentivi'
 copyright = '2020, Duy V. Huynh'
 author = 'Duy V. Huynh'
-version = '0.1.0'
-release = '0.1.0'
+version = '1.0.4'
+release = '1.0.4'
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
